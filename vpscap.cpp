1676165dd80e84dd50f15615f1a1137bff2ae02c
siddhusathu20 <53175949+siddhusathu20@users.noreply.github.com>
2024-07-26 18:34:45 +0530

    Update vpscap.cpp

--- vpscap.cpp
+++ vpscap.cpp
@@ -9,6 +9,6 @@ int main() {
   std::cout << "\nEnter password: ";
   std::cin >> pwd;
   if (usrn=="smadev" && pwd=="transrcd") {
-    std::cout << "\nVPSC Administrator Panel - New Notice\nThe Fires of Entia Episode One out now: https://github.com/siddhusathu20/thefiresofentia/blob/main/The%20Fires%20of%20Entia%20-%20Episode%20One.pdf";
+    std::cout << "\nVPSC Administrator Panel - New Notice\nThanks for checking this out, though I've made much better projects since then and you should probably look at those!";
   }
 } 
